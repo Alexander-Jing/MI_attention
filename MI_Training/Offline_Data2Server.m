f54d1f9360d061963eb61f22cb660b309d5c38f8
Jing <18061523086@163.com>
2023-08-16 23:01:15 +0800

    preparing the socket between matlab and python sever for larger matrix, from computer on the 12th floor, 2023.8.16

--- MI_Training/Offline_Data2Server.m
+++ MI_Training/Offline_Data2Server.m
@@ -1,16 +1,54 @@
-%% 远程主机为localhost，即本地主机，要连接的目的端口为30000，作为客户机使用
-client=tcpip('172.18.22.21',8888,'NetworkRole','client');
- 
-%% 设置接收和发送缓存区的最大容量，这里设置的是1000*1000*8，也就是一个1000*1000的double类型的数组大小
-client.InputBuffersize=8000000;
-client.OutputBuffersize=8000000;
- 
-%% 打开连接，寻找目的服务器，如果未找到，报错
-fopen(client);
-text = 'Fanshi';
+close all;clear;clc;
+data = double(rand(2048,2048));
+config = whos('data');
+time_out = 60; % 投送数据包的等待时间
+tcpipClient = tcpip('172.18.22.21', 8888,'NetworkRole','Client');
+set(tcpipClient,'OutputBufferSize',67108880+64);%2048*4096
+set(tcpipClient,'Timeout',time_out);
+tcpipClient.InputBufferSize = 8388608;%8M
+tcpipClient.ByteOrder = 'bigEndian';
+fopen(tcpipClient);
+disp("连接成功")
+disp("数据发送")
 
-%% 向服务器发送数据
-while 1
-    pause(1)
-    fprintf(client,text);%发送文本数据
+send_data = [config.size(:);data(:)];
+config_send = whos('send_data');   % whos('send_data')将返回该变量的名称、大小、字节数、类型等信息
+fwrite(tcpipClient,[config_send.bytes/2;send_data],'float32');  % 这里matlab的double是8个字节，然后这里使用的4字节的float32传输，所以config_send.bytes要除以2，表示使用4字节的float32形式传输用了多少个字节
+disp("数据接收")
+recv_data = [];
+
+%重复多次接收
+h=waitbar(0,'正在接收数据');
+while isempty(recv_data)
+    recv_data=fread(tcpipClient);%读取第一组数据
 end
+header = convertCharsToStrings(native2unicode(recv_data,'utf-8'));
+recv_bytes = str2double(regexp(header,'(?<=(L": )).*?(?=(,|$))','match'))-2;%正则化提取数据大小
+while length(recv_data)<recv_bytes
+    if recv_data(end)==125
+        break
+    end
+    waitbar(length(recv_data)/recv_bytes)
+    recv_package = [];
+    while isempty(recv_package)
+        try
+            recv_package=fread(tcpipClient);
+        catch
+            continue
+        end
+    end
+    recv_data = vertcat(recv_data,recv_package);
+end
+close(h)
+chararray = native2unicode(recv_data,'utf-8');
+str = convertCharsToStrings(chararray);
+try
+    U = dic.U;
+    S = dic.S;
+    V = dic.V;
+    re = U*diag(S)*V;
+catch
+    disp('WARNNING:接收不完全')
+end
+disp('连接断开')
+fclose(tcpipClient);
