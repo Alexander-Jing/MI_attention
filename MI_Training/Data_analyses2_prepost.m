095fd1d98e91fa8a23a1a74d0483fa94662a42dd
Alexcander-Jing <18061523086@163.com>
2024-03-11 11:26:43 +0800

    pre post data analysis for the topo painting, from computer in 422, 2024.3.11

--- MI_Training/Data_analyses2_prepost.m
+++ MI_Training/Data_analyses2_prepost.m
@@ -1,10 +1,13 @@
-%% 数据读取
-subject_name_online =  'Jyt_test_0310_online'; % 'Jyt_test_0101_online';%'Jyt_test_0108_online'; %%  %  % 閻炴凹鍋夐惁顖涙叏閹捐櫕鍊?
-sub_online_collection_folder = 'Jyt_test_0310_online_20240310_210638053_data'; %'Jyt_test_0101_online_20240101_175129548_data';  % %'';%  %'Jyt_test_0108_online_20240110_000906267_data'; %   %''; %  % 
-sub_online_rawdata_file = 'Online_EEGMI_RawData_1_Jyt_test_0310_online20240310_212430066.mat'; %'Online_EEGMI_RawData_1_Jyt_test_0101_online20240101_181405221';  %;;  
+%% 被试名称和实验的文件夹
+root_path = 'F:\CASIA\MI_engagement\MI_attention\MI_Training';  % 根目录用于存储数据和分析
+subject_name_post = 'Jyt_test_0310_post_control'; %'Jyt_test_0101_1_online';% 'Jyt_test_0101_online'; %  % 被试姓名
+sub_post_collection_folder = 'Jyt_test_0310_post_control_20240310_212613989_data';
 
-subject_name_offline =  'Jyt_test_0310_offline';   
-sub_offline_collection_folder = 'Jyt_test_0310_offline_20240310_195952653_data';  
+subject_name_pre = 'Jyt_test_0310_pre_control';
+sub_pre_collection_folder = 'Jyt_test_0310_pre_control_20240310_204214585_data';
+
+subject_name_offline =  'Jyt_test_0310_offline';  % 离线收集数据时候的被试名称
+sub_offline_collection_folder = 'Jyt_test_0310_offline_20240310_195952653_data';  % 被试的离线采集数据
 
 %channels_preprocessing = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15, 16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32];   
 channels = [1,2,3,4,5,6,7,8,9,10,11,12,13,  15, 16,17,18,19,  21,22,23,24,25,26,27,28,29,30,31,32]; 
@@ -16,98 +19,49 @@ motorclasses = 3;
 channels_num = length(channels);
 
 %% 数据读取
-subject_rawdata_folder = ['.\', sub_online_collection_folder, '\' 'Online_EEGMI_RawData_', subject_name_online];
-rawdata = load([subject_rawdata_folder, '\', sub_online_rawdata_file]);
-%rawdata_comparison = load('F:\CASIA\MI_engagement\MI_attention\MI_Training\Jyt_test_0131_comparison_20240131_194732925_data\Offline_EEGMI_RawData_Jyt_test_0131_comparison\Offline_EEGMI_RawData_Jyt_test_0131_comparison20240131_195537284.mat');
-rawdata_comparison = load('F:\CASIA\MI_engagement\MI_attention\MI_Training\Jyt_test_0310_post_control_20240310_212613989_data\Offline_EEGMI_RawData_Jyt_test_0310_post_control\Offline_EEGMI_RawData_Jyt_test_0310_post_control20240310_213147853.mat');
-%rawdata_offline = load('F:\CASIA\MI_engagement\MI_attention\MI_Training\Jyt_test_0131_offline_20240131_204044614_data\Offline_EEGMI_RawData_Jyt_test_0131_offline\Offline_EEGMI_RawData_Jyt_test_0131_offline20240131_205742247.mat');%load('F:\CASIA\MI_engagement\MI_attention\MI_Training\Jyt_test_0101_1_offline_20240101_193332077_data\Offline_EEGMI_RawData_Jyt_test_0101_1_offline\Offline_EEGMI_RawData_Jyt_test_0101_1_offline20240101_194900541.mat', 'TrialData');
+rawdata_pre_folder = fullfile(root_path, sub_pre_collection_folder,['Offline_EEGMI_RawData_', subject_name_pre]);
+rawdata_pre_file = dir(fullfile(rawdata_pre_folder, '*.mat'));
+rawdata_pre = load(fullfile(rawdata_pre_folder, rawdata_pre_file.name));
+rawdata_pre = rawdata_pre.TrialData;
 
-rawdata = rawdata.TrialData;
-rawdata_comparison = rawdata_comparison.TrialData;
-%rawdata_offline = rawdata_offline.TrialData;
-
-%rawdata = rawdata;
-%rawdata = rawdata_offline;
-rawdata = rawdata_comparison;
+rawdata_post_folder = fullfile(root_path, sub_post_collection_folder,['Offline_EEGMI_RawData_', subject_name_post]);
+rawdata_post_file = dir(fullfile(rawdata_post_folder, '*.mat'));
+rawdata_post = load(fullfile(rawdata_post_folder, rawdata_post_file.name));
+rawdata_post = rawdata_post.TrialData;
 %% 数据处理
 sample_frequency = 256; 
 
 WindowLength = 512;  
 SlideWindowLength = 256;  
 
-Trigger = double(rawdata(end,:)); 
+[Idle_all_pre, MIDrinking_all_pre, MIPouring_all_pre] = process_data(rawdata_pre, sample_frequency, WindowLength, SlideWindowLength, ...
+    channels, channels_num);
+[Idle_all_post, MIDrinking_all_post, MIPouring_all_post] = process_data(rawdata_post, sample_frequency, WindowLength, SlideWindowLength, ...
+    channels, channels_num);
 
-class_index = 6;
-RawDataIdle = double(rawdata(1:end-1, Trigger == class_index));  
-FilteredDataIdle = DataFilter(RawDataIdle, sample_frequency);  
-[windows_per_session, SampleDataPre] = WindowsDataPre(FilteredDataIdle, WindowLength, SlideWindowLength);
-[DataSampleIdle, ] = DataWindows(SampleDataPre, FilteredDataIdle, channels, class_index, windows_per_session, SlideWindowLength, WindowLength);
-
-class_index = 1;
-RawDataMI_Drinking = double(rawdata(1:end-1, Trigger == class_index));  
-FilteredDataMI_Drinking = DataFilter(RawDataMI_Drinking, sample_frequency);  
-[windows_per_session, SampleDataPre] = WindowsDataPre(FilteredDataMI_Drinking, WindowLength, SlideWindowLength);
-[DataSampleMI_Drinking, ] = DataWindows(SampleDataPre, FilteredDataMI_Drinking, channels, class_index, windows_per_session, SlideWindowLength, WindowLength);
-
-class_index = 2;
-RawDataMI_Pouring = double(rawdata(1:end-1, Trigger == class_index));  
-FilteredDataMI_Pouring = DataFilter(RawDataMI_Pouring, sample_frequency);  
-[windows_per_session, SampleDataPre] = WindowsDataPre(FilteredDataMI_Pouring, WindowLength, SlideWindowLength);
-[DataSampleMI_Pouring, ] = DataWindows(SampleDataPre, FilteredDataMI_Pouring, channels, class_index, windows_per_session, SlideWindowLength, WindowLength);
-
-%% 计算均值
-Idle_all = zeros(channels_num, 1);
-MIDrinking_all = zeros(channels_num, 1);
-MIPouring_all = zeros(channels_num, 1);
-
-all_eegdata_idle = zeros(channels_num, size(DataSampleIdle, 2));
-for cell_idx = 1 : size(DataSampleIdle, 2)
-    eegdata_idle = zeros(channels_num, 1);
-    data_filtered = DataSampleIdle{cell_idx}(:, :);
-    for channel_idx = 1 : channels_num
-        [pxx, f] = pwelch(data_filtered(channel_idx, :), hanning(128), 64, 128, 256);
-        eegdata_idle(channel_idx)=10 * log10(mean(pxx));
-    end
-    all_eegdata_idle(:, cell_idx) = eegdata_idle;
-end
-Idle_all(:, 1) = mean(all_eegdata_idle, 2);
-
-all_eegdata_MI_Drinking = zeros(channels_num, size(DataSampleMI_Drinking, 2));
-for cell_idx = 1 : size(DataSampleMI_Drinking, 2)
-    eegdata_MI_Drinking = zeros(channels_num, 1);
-    data_filtered = DataSampleMI_Drinking{cell_idx}(:, :);
-    for channel_idx = 1 : channels_num
-        [pxx, f] = pwelch(data_filtered(channel_idx, :), hanning(128), 64, 128, 256);
-        eegdata_MI_Drinking(channel_idx)=10 * log10(mean(pxx));
-    end
-    all_eegdata_MI_Drinking(:, cell_idx) = eegdata_MI_Drinking;
-end
-MIDrinking_all(:, 1) = mean(all_eegdata_MI_Drinking, 2);
-
-all_eegdata_MI_Pouring = zeros(channels_num, size(DataSampleMI_Pouring, 2));
-for cell_idx = 1 : size(DataSampleMI_Pouring, 2)
-    eegdata_MI_Pouring = zeros(channels_num, 1);
-    data_filtered = DataSampleMI_Pouring{cell_idx}(:, :);
-    for channel_idx = 1 : channels_num
-        [pxx, f] = pwelch(data_filtered(channel_idx, :), hanning(128), 64, 128, 256);
-        eegdata_MI_Pouring(channel_idx)=10 * log10(mean(pxx));
-    end
-    all_eegdata_MI_Pouring(:, cell_idx) = eegdata_MI_Pouring;
-end
-MIPouring_all(:, 1) = mean(all_eegdata_MI_Pouring, 2);
-
-% 第一张图
-subplot(1,2,1);
-topo_painting = MIDrinking_all-Idle_all;
+% 第1张图
+subplot(2,2,1);
+topo_painting = MIDrinking_all_pre-Idle_all_pre;
 topoplot([topo_painting;],'Cap30_1.locs','maplimits',[-1.0,1.0], 'electrodes', 'on', 'style', 'map');
-title('MIDrinking');
+title('MIDrinking pre');
 
-% 第二张图
-subplot(1,2,2);
-topo_painting = MIPouring_all-Idle_all;
+% 第2张图
+subplot(2,2,2);
+topo_painting = MIPouring_all_pre-Idle_all_pre;
 topoplot([topo_painting;],'Cap30_1.locs','maplimits',[-1.0,1.0], 'electrodes', 'on', 'style', 'map');
-title('MIPouring');
+title('MIPouring pre');
 
+% 第3张图
+subplot(2,2,3);
+topo_painting = MIDrinking_all_post-Idle_all_post;
+topoplot([topo_painting;],'Cap30_1.locs','maplimits',[-1.0,1.0], 'electrodes', 'on', 'style', 'map');
+title('MIDrinking post');
+
+% 第4张图
+subplot(2,2,4);
+topo_painting = MIPouring_all_post-Idle_all_post;
+topoplot([topo_painting;],'Cap30_1.locs','maplimits',[-1.0,1.0], 'electrodes', 'on', 'style', 'map');
+title('MIPouring post');
 %% 计算均值的处理函数
 function [Idle_all, MIDrinking_all, MIPouring_all] = process_data(rawdata, sample_frequency, WindowLength, SlideWindowLength, channels, channels_num)
     % 数据处理
